110758a9db6f1d72445f815c46be006a3e187108
Haopeng Song <haopengs@usc.edu>
2019-02-11 20:32:35 -0800

    handler search

--- service/main.go
+++ service/main.go
@@ -1,27 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
 )
+
 type Location struct {
 	Lat float64 `json:"lat"`
 	Lon float64 `json:"lon"`
 }
 
 type Post struct {
-	User string `json:"user"`
-	Message string `json:"message"`
+	User     string   `json:"user"`
+	Message  string   `json:"message"`
 	Location Location `json:"location"`
 }
 
 func main() {
 	fmt.Println("started-service")
-    http.HandleFunc("/post", handlerPost)
+	http.HandleFunc("/post", handlerPost)
+	http.HandleFunc("/search", handlerSearch)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
+}
+
+func handlerSearch(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received one request for search")
+	lat := r.URL.Query().Get("lat")
+	lon := r.URL.Query().Get("lon")
+	fmt.Fprintf(w, "Search received: %s %s", lat, lon)
 }
 
 func handlerPost(w http.ResponseWriter, r *http.Request) {
